260ddae0a50e7dc1868f1f299f926a7b1c65bdd7
ylxdzsw <ylxdzsw@gmail.com>
2017-08-04 17:00:43 +0800

    change wechat to python

---
+++ console/wechat.py
@@ -0,0 +1,24 @@
+import itchat
+import redis
+import time
+
+@itchat.msg_register(itchat.content.TEXT, isFriendChat=True, isGroupChat=True, isMpChat=True)
+def echo(msg):
+    if "alive" in msg['Text']:
+        return "I'm still alive"
+
+itchat.auto_login(hotReload=True, statusStorageDir="/tmp/piggy.wechat.pkl")
+itchat.run(blockThread=False)
+
+room = itchat.search_chatrooms("coineering")[0]["UserName"]
+
+db = redis.StrictRedis(db=1)
+
+channel = db.pubsub()
+channel.subscribe(**{'channel/wechat.msg': lambda x: itchat.send_msg(x["data"].decode(), room)})
+
+channel.run_in_thread(sleep_time=0.001)
+
+while True:
+    db.set("status/wechat.alive", '', 25)
+    time.sleep(20)
